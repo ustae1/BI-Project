626745840a93fd74f2f6231ac97e3f8b9c91814c
minhm <minhman.tran@students.bfh.ch>
2023-01-02 16:58:36 +0100

    cleaned data

--- app.R
+++ app.R
@@ -8,6 +8,30 @@
 #
 
 library(shiny)
+library(dplyr)
+bc <- read.csv("C:\\Users\\minhm\\Documents\\RWorkSpace\\BI-Project\\METABRIC_RNA_Mutation.csv", stringsAsFactors = FALSE)
+
+# Datenbereinigung
+ds <- bc[0:31]
+summary(ds)
+ds[ds == ''] <- NA
+na_cols <- is.na(ds)
+ds <- replace(ds, na_cols, "Unknown")
+
+ds_clean <- ds %>%
+  mutate_if(sapply(ds, is.character), as.factor)
+
+ds_clean$chemotherapy <- factor(ds_clean$chemotherapy, levels = c(0, 1), labels = c("No", "Yes"))
+ds_clean$hormone_therapy <- factor(ds_clean$hormone_therapy, levels = c(0, 1), labels = c("No", "Yes"))
+ds_clean$overall_survival <- factor(ds_clean$overall_survival, levels = c(0, 1), labels = c("No", "Yes"))
+ds_clean$radio_therapy <- factor(ds_clean$radio_therapy, levels = c(0, 1), labels = c("No", "Yes"))
+ds_clean$cohort <- factor(ds_clean$cohort)
+ds_clean$tumor_size <- as.numeric(ds_clean$tumor_size)
+ds_clean$mutation_count <- as.numeric(ds_clean$mutation_count)
+
+str(ds_clean)
+sum(is.na(ds_clean))
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
